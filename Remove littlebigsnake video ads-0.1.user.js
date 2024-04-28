72c436955339ad469bfbe8909ae0094ae73e087d
Andrew Allen <a2intl@users.noreply.github.com>
2024-04-27 19:53:57 -0700

    Create Remove littlebigsnake video ads-0.1.user.js

---
+++ Remove littlebigsnake video ads-0.1.user.js
@@ -0,0 +1,51 @@
+// ==UserScript==
+// @name         Remove littlebigsnake video ads
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  remove video ads
+// @author       You
+// @match        https://littlebigsnake.com/
+// @grant        all
+// ==/UserScript==
+
+var lastskip = null;
+
+function skipVideo(v) {
+  //v.src='file:///home/a2/smallvideo.mp4'
+  if(v != lastskip)
+  {
+    v.addEventListener('canplay', (event) => { skipVideo(event.target) });
+    v.addEventListener('playing', (event) => { skipVideo(event.target) });
+    lastskip=v;
+  }
+
+  console.log('skipping video!');
+  v.playbackRate=15;
+  v.play();
+  try {
+    if(v.currentTime<v.duration-1) v.currentTime=v.duration;
+  } catch(e) {
+  }
+  var c = document.getElementById('adsContainer');
+  if(c) c.style.display='none';
+}
+
+function mutCallback(mutationList, observer) {
+  for(const m of mutationList) {
+    for(const n of m.addedNodes) {
+      var tag = n.tagName && n.tagName.toLowerCase();
+      if(tag === 'video') {
+	    skipVideo(n);
+      }
+      if(tag === 'lima-video' && n.shadowRoot) {
+	    for(const v of n.shadowRoot.querySelectorAll('video')) {
+	      skipVideo(v);
+	    }
+      }
+    }
+  }
+}
+
+var mutObs = new MutationObserver(mutCallback);
+
+mutObs.observe(document, {childList:true, subtree:true});
